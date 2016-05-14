96ac861bc3f25682e0684f10ec83d4c2104f7bd1
Peter Reid <peter.d.reid@gmail.com>
2016-05-14 11:46:06 -0400

    Update documentation

---
+++ implementors/core/ops/trait.Fn.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['schedule_recv'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
